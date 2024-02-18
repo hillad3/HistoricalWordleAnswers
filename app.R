2356a3a291382666d431c9c4595838b7efd9e81e
hillad3 <adam.x.hill@gmail.com>
2024-02-18 15:48:13 -0500

    Minor formatting of HTML p tags

--- app.R
+++ app.R
@@ -100,11 +100,14 @@ ui <- page_fluid(
       br(),
       column(
         8,
-        tags$span("This Scrabble word list is provided from the {scrabble} package by Julie Laffy available on "),
+        tags$span("This Scrabble word list is provided from the {scrabble} package by Julie Laffy
+                  available on "),
         tags$a(href="https://github.com/jlaffy/scrabble", "github", style = "color:#3BC143", .noWS="after"),
-        tags$span(". For those familiar with both games, clearly the criteria for a Wordle answer is not the same as a Scrabble word."),
+        tags$span(". For those familiar with both games, clearly the criteria for a Wordle answer is
+                  not the same as a Scrabble word."),
         tags$span("Use your best judgement if you are looking for inspiration."),
-        tags$span("(I make no judgements about how or if you use this list when playing Wordle. Let's all enjoy games the way we prefer to play them.)"),
+        tags$span("(I make no judgements about how or if you use this list when playing Wordle.
+                  Let's all enjoy games the way we prefer to play them.)"),
       ),
       br(),
       modWordListUI("scrabble",five_letter_words,years,dups_present,days_since_last_update,FALSE,FALSE)
@@ -117,20 +120,26 @@ ui <- page_fluid(
         p(
           tags$span("Occasionally, in the puzzle game "),
           tags$a(href="https://www.nytimes.com/games/wordle","Wordle", style = "color:#3BC143", .noWS="after"),
-          tags$span(", I find myself wanting to know if a word was previously used or if a Wordle answer has ever been repeated."),
+          tags$span(", I find myself wanting to know if a word was previously used or if a Wordle
+                    answer has ever been repeated."),
           tags$span(" Ya know, fun stuff you think about while riding a rollercoaster.")
         ),
         p(),
-        p("I'm not a fan of visiting ad-bloated websites to wade through multi-paragraph lead-ins (like this one?) only to have to scan an ever growing list of words. So, I decided to make a lightweight and data-focused website that can help me in this endeavour -- and now also you!"),
+        p("I'm not a fan of visiting ad-bloated websites to wade through multi-paragraph lead-ins
+          (like this one?) only to have to scan an ever growing list of words. So, I decided to make
+          a lightweight and data-focused website that can help me in this endeavour -- and now also you!"),
         p(
-          tags$span("For the R enthusiasts, this website was built using the {shiny} package in R, along with Bootstrap 5 using {bslib}."),
+          tags$span("For the R enthusiasts, this website was built using the {shiny} package in R Studio,
+                    along with Bootstrap 5 using {bslib}."),
           tags$span("Tables were created with the {data.table} package and rendered with the {DT} package."),
           tags$span("The interactive graphs were created in {plotly} using tokenization with {tidytext}."),
           tags$span("Its code is available on my github, "),
           tags$a(href="https://github.com/hillad3/HistoricalWordleAnswers", "here", style = "color:#3BC143", .noWS="after"),
           tags$span(".")
         ),
-        p("I plan to update this list every couple of weeks, assuming the hubbub of life doesn't get the best of me. If I can figure out a way to create a feedback form, I'll add one and then you can nag me to update."),
+        p("I plan to update this list every couple of weeks, assuming the hubbub of life doesn't
+          get the best of me. If I can figure out a way to create a feedback form, I'll add one and
+          then you can nag me to update."),
         tags$span("Happy Wordle-ing!", style="font-weight:bold; font-size:110%"),
         tags$span(" (let's pretend that's a thing normal people say)", style="font-size:80%"),
         p("", style="margin-bottom:25px")
