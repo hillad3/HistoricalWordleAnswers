1ddf3a49c764813f19aba4ed5c2fef106f38bed3
hillad3 <adam.x.hill@gmail.com>
2024-02-19 10:57:28 -0500

    Procedure to update database if local file has additional words.

---
+++ updateDBTable.R
@@ -0,0 +1,33 @@
+rm(list = ls())
+gc()
+
+library(data.table)
+library(dplyr)
+library(DBI)
+
+local_ans <- fread("data/wordle_answers.csv")
+
+con <- dbConnect(
+  drv = RPostgres::Postgres(),
+  dbname = Sys.getenv("dbname"),
+  host = Sys.getenv("host"),
+  port = Sys.getenv("port"),
+  user = Sys.getenv("userid"),
+  password = Sys.getenv("pwd")
+)
+db_ans <- dbReadTable(con, "wordle") |> as.data.table()
+
+new_ans <- local_ans[!(Word %in% db_ans$Word)]
+
+if(dim(new_ans)[1]==0){
+  # do nothing and close connection
+  dbDisconnect(con)
+} else {
+  # create temp table and then insert into
+
+  dbWriteTable(con, "tmp", new_ans, overwrite=TRUE)
+  dbWriteTable(con, "wordle", "tmp", append=TRUE)
+  dbRemoveTable(con, "tmp")
+  dbDisconnect(con)
+
+}
