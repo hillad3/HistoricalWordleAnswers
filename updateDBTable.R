4ebc5b2ca3263ab82828baa7ced6275b95194e36
hillad3 <adam.x.hill@gmail.com>
2024-03-17 14:40:39 -0400

    Added functionality for complete overwrite of table.

--- updateDBTable.R
+++ updateDBTable.R
@@ -19,13 +19,19 @@ db_ans <- dbReadTable(con, "wordle") |> as.data.table()
 
 new_ans <- local_ans[!(Word %in% db_ans$Word)]
 
+overwrite_table <- TRUE
+
 if(dim(new_ans)[1]==0){
   # do nothing and close connection
   dbDisconnect(con)
-} else {
-  # create temp table and then insert into
+} else if (overwrite_table) {
+  # overwrite table
 
-  dbWriteTable(con, "wordle", new_ans, append=TRUE)
+  dbWriteTable(con, "wordle", local_ans,  overwrite=TRUE)
   dbDisconnect(con)
 
+} else {
+
+  dbWriteTable(con, "wordle", db_ans, new_ans, append=TRUE)
+  dbDisconnect(con)
 }
