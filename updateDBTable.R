6d33738086fbf22dd5abcee6cc862b2549664778
hillad3 <adam.x.hill@gmail.com>
2024-05-22 21:21:09 -0400

    Minor refactor to join Wordle Answers with 5-letter scrabble answers and then loading into database.

--- updateDBTable.R
+++ updateDBTable.R
@@ -7,6 +7,11 @@ library(DBI)
 
 local_ans <- fread("data/wordle_answers.csv")
 
+five_letter_words <- fread("data/five_letter_scrabble_words.csv")
+five_letter_words[,Word:=toupper(Word)]
+
+local_ans <- local_ans[five_letter_words[,Index:=NULL], on = 'Word']
+
 con <- dbConnect(
   drv = RPostgres::Postgres(),
   dbname = Sys.getenv("dbname"),
