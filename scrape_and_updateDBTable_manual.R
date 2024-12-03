3aedf852be56165da469d1f79215ca6fa334f171
hillad3 <adam.x.hill@gmail.com>
2024-12-02 23:01:10 -0500

    Updated lubridate steps since database is now storing date column as date data type.

--- scrape_and_updateDBTable_manual.R
+++ scrape_and_updateDBTable_manual.R
@@ -21,9 +21,9 @@ update_db <- function(){
 
   # finally merge wordle answers with scrabble answers to get one full table
   website_word_list <- five_letter_scrabble_words |>
-    full_join(wordle_answers, by = c("Word")) |>
-    arrange(Word) |>
-    select(Index, Date, Word)
+    full_join(wordle_answers, by = c("word")) |>
+    arrange(word) |>
+    select(index, date, word)
 
   dbWriteTable(con, "website_word_list", website_word_list |> collect(), overwrite=TRUE)
 
@@ -32,9 +32,9 @@ update_db <- function(){
 
 # make a connection to the existing wordle answer table and get max index and corresponding date
 db_wordle_answers <- dbReadTable(con, "wordle_answers") |> as.data.table()
-setorder(db_wordle_answers, -Index)
-max_index <- max(db_wordle_answers$Index)
-max_date <- db_wordle_answers[Index==max_index,.(Date)] |> unlist() |> mdy()
+setorder(db_wordle_answers, -index)
+max_index <- max(db_wordle_answers$index)
+max_date <- db_wordle_answers[index==max_index][,date] |> unlist()
 
 
 if( (ymd(today())-1 > max_date)){
@@ -43,30 +43,30 @@ if( (ymd(today())-1 > max_date)){
   u <- read_html("https://wordfinder.yourdictionary.com/wordle/answers/")
   url_wordle_answers <- u |> html_elements("table") |> html_table()
   url_wordle_answers <- bind_rows(url_wordle_answers) |> as.data.table()
-  names(url_wordle_answers) <- c("Date","Index","Word")
+  names(url_wordle_answers) <- c("date","index","word")
 
   # remove today's word since it would require javascript to manipulate the DOM
   url_wordle_answers <- url_wordle_answers[2:dim(url_wordle_answers)[1]]
-  url_wordle_answers <- merge(url_wordle_answers, db_wordle_answers[Index<1200], by = c("Index","Word"), all.x=TRUE)
-  url_wordle_answers[,Date.y:=mdy(Date.y)]
+  url_wordle_answers <- merge(url_wordle_answers, db_wordle_answers, by = c("index","word"), all.x=TRUE)
+  # url_wordle_answers[,date.y:=mdy(date.y)]
 
   # I found this easier to do with a data.frame since data.table was converting dates to numbers
   url_wordle_answers <- url_wordle_answers |>
     as.data.frame() |>
     mutate(
-      Date = if_else(is.na(Date.y),min(Date.y[!is.na(Date.y)])+Index,Date.y)
+      date = if_else(is.na(date.y),min(date.y[!is.na(date.y)])+index,date.y)
     ) |>
     as.data.table()
 
-  url_wordle_answers[,Date.x:=NULL]
-  url_wordle_answers[,Date.y:=NULL]
+  url_wordle_answers[,date.x:=NULL]
+  url_wordle_answers[,date.y:=NULL]
   # ensure wordle answers are in MM/DD/YYYY format to be compatible with shiny app
-  url_wordle_answers[,`Date`:=format(`Date`, format = "%m/%d/%Y")]
+  # url_wordle_answers[,`date`:=format(`date`, format = "%m/%d/%Y")]
 
-  setcolorder(url_wordle_answers, neworder = c("Index", "Date", "Word"))
-  setorder(url_wordle_answers, -Index)
+  setcolorder(url_wordle_answers, neworder = c("index", "date", "word"))
+  setorder(url_wordle_answers, -index)
 
-  new_wordle_answers <- url_wordle_answers[Index > max_index]
+  new_wordle_answers <- url_wordle_answers[index > max_index]
 
   if(dim(new_wordle_answers)[1]>0){
     print("Adding new wordle answers to table in the database...")
